637871f0ae33a0b80cfc81127ece06400328ebc9
cipherbikram <161165118+Cipherbikramdey@users.noreply.github.com>
2025-08-15 23:54:47 +0530

    Delete tools directory

--- tools/http_headers.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import sys, requests
-if len(sys.argv) != 2:
-    print("Usage: http_headers.py <url>")
-    sys.exit(1)
-r = requests.get(sys.argv[1])
-for k,v in r.headers.items():
-    print(f"{k}: {v}")
