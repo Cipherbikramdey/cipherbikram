5f82c421efe6f7aa03fad77cb82ada110a43e66c
cipherbikram <161165118+Cipherbikramdey@users.noreply.github.com>
2025-08-16 13:38:38 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-const CACHE='cb-site-v1';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['index.html','assets/css/style.css','assets/js/main.js'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
